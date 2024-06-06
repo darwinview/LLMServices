669f1344176df4e65ced75dc91107c08cd74d774
darwinview <164188263+darwinview@users.noreply.github.com>
2024-06-06 16:58:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 9002;
+
+app.use(bodyParser.json());
+
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
+
+const OPENAI_URL = "https://talentintelligence.openai.azure.com/";
+const OPENAI_KEY = "a3611284de804ee6a395fd7e2827ec18";
+const model = "gpt4";
+
+const client = new OpenAIClient(
+    OPENAI_URL,
+    new AzureKeyCredential(OPENAI_KEY)
+);
+
+// Route for LLM action
+app.post('/llm', (req, res) => {
+
+    // Get data from request body
+    // const messages = req.body;
+
+    console.log("Invoked");
+
+    
+
+    // const temperature = 1;
+
+    // async function getChatCompletion() {
+    //     try {
+
+    //         const result = await client.getChatCompletions(
+    //             model,
+    //             messages,
+    //             {
+    //                 maxTokens: 4000,
+    //                 temperature: temperature,
+    //                 stop: null,
+    //                 top_p: 0.95,
+    //                 frequency_penalty: 0,
+    //                 presence_penalty: 0
+    //             }
+    //         );
+
+    //         const assistantMessage = result.choices[0].message.content;
+    //         console.log(assistantMessage);
+
+    //         res.json(assistantMessage)
+
+    //     } catch (error) {
+    //         res.json(error);
+    //     }
+    // }
+
+    // getChatCompletion();
+
+    res.json("invoked");
+    
+});
+
+app.listen(port, () => {
+    console.log('server running at https://localhost:' + port);
+});
+
+ 
